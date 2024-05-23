91adcc2b96baf599fd71a8398e2ea7601bcc8be2
Альгиз Ишимов <algiz.i59@mail.ru>
2024-05-23 23:20:07 +0300

    Начали делать вход и регистрацию

---
+++ TeamWorkWebApp/Interfaces/IAppRepository.cs
@@ -0,0 +1,16 @@
+﻿using TeamWorkWebApp.Models;
+
+namespace TeamWorkWebApp.Interfaces
+{
+    public interface IAppRepository
+    {
+        Task<IEnumerable<User>> GetUsersAsync();
+        Task<IEnumerable<Group>> GetGroupsAsync();
+        Task<IEnumerable<Models.Task>> GetTasksAsync();
+
+
+
+
+
+    }
+}
---
+++ TeamWorkWebApp/ViewModels/AccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamWorkWebApp.ViewModels
+{
+    public class AccountViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+        public string Name { get; set; }
+    }
+}
+
