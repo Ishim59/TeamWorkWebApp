bb1ab2288d406e7a167ec69db02705589e1f04ac
Альгиз Ишимов <algiz.i59@mail.ru>
2024-06-01 09:22:41 +0300

    tam s9m

--- TeamWorkWebApp/ViewModels/AccountViewModel.cs
+++ TeamWorkWebApp/ViewModels/AccountViewModel.cs
@@ -5,7 +5,9 @@ namespace TeamWorkWebApp.ViewModels
     public class AccountViewModel
     {
         public string Email { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [DataType(DataType.Password)]
         public string PasswordConfirmation { get; set; }
         public string Name { get; set; }
     }
